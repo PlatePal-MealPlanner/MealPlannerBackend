cc785cc1ab7e267a3ce21db4371dea19fdfce431
John Rey Cutab <96228393+JohnRey7@users.noreply.github.com>
2024-11-14 21:36:14 +0800

    Update AuthController.java

--- src/main/java/com/g1appdev/mealplanner/authenticator/AuthController.java
+++ src/main/java/com/g1appdev/mealplanner/authenticator/AuthController.java
@@ -2,12 +2,14 @@ package com.g1appdev.mealplanner.authenticator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
